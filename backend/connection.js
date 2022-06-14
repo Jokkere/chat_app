287dbf9bd177f1e603dc224643418688f3d0e970
Jokkere <39446379+Jokkere@users.noreply.github.com>
2022-06-14 23:43:47 +0200

    Untracked files issue resolved to fix .gitignore

--- backend/connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.yyeefvo.mongodb.net/?retryWrites=true&w=majority`, ()=> {
-  console.log('Połączono z mongodb')
-})
